1b22619520b27dda6feeb3c2b71a595f232e9efb
Dominic Montelongo <montelnogodominic@gmail.com>
2019-04-05 11:46:03 -0700

    Take break to concentrate on garden, social media, and other news; create js array for second attempt at project

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,26 @@
 function beepBoop(i){
-  for (var i = 0;i <= 100;1++) {
-    if (i === 32) {
-      console.log ("I'm Sorry, Dave.  I'm afraid I can't do that.");
-    } else if (i === 21) {
-      console.log ('Boop!');
-    } else if (i === 13) {
-      console.log ("I'm Sorry, Dave.  I'm afraid I can't do that.");
-    } else if ((i === 1) && () {
-      console.log ('Beep!');
-    } else if (i === 2) {
-      console.log ('Boop!');
-    } else if (i === 3) {
-      console.log ("I'm Sorry, Dave.  I'm afraid I can't do that.");
-
-    if
-
-  console.log ('100')
-  console.log ('Beep!');
-  console.log ('Boop!');
-  console.log ("I'm Sorry, Dave.  I'm afraid I can't do that.")
-  console.log(i)
-    }
-  };
+var nums = [1,2,3,11,13,32];
+  // Everything below this was created under the intention of making output based misinterpretation of assignment.  Info retained for proof of working during project time.
+  // for (var i = 0;i <= 100;1++) {
+  //   if (i === 32) {
+  //     console.log ("I'm Sorry, Dave.  I'm afraid I can't do that.");
+  //   } else if (i === 21) {
+  //     console.log ('Boop!');
+  //   } else if (i === 13) {
+  //     console.log ("I'm Sorry, Dave.  I'm afraid I can't do that.");
+  //   } else if ((i === 1) && () {
+  //     console.log ('Beep!');
+  //   } else if (i === 2) {
+  //     console.log ('Boop!');
+  //   } else if (i === 3) {
+  //     console.log ("I'm Sorry, Dave.  I'm afraid I can't do that.");
+  //
+  //   if
+  //
+  // console.log ('100')
+  // console.log ('Beep!');
+  // console.log ('Boop!');
+  // console.log ("I'm Sorry, Dave.  I'm afraid I can't do that.")
+  // console.log(i)
+  //   }
+  // };

6870ce3e85b203a621c049565677e18b45ccaff0
Dominic Montelongo <montelnogodominic@gmail.com>
2019-04-05 10:04:17 -0700

    Add js console.logs

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
 function beepBoop(number){
-  for (var i = 0; i <= 100r;1++){
+  for (var i = 0; i <= 100;1++){
     // if ()
-  // }
-
+  }
+console.log ('Beep!');
+console.log ('Boop!');
+console.log ("I'm Sorry, Dave.  I'm afraid I can't do that.")
 console.log(i)
-}
+};

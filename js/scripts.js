95b508422885bc9a7d43f2281e1d9987b0a17196
Dominic Montelongo <montelnogodominic@gmail.com>
2019-04-05 10:39:43 -0700

    Add js else/if statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 function beepBoop(i){
-  for (var i = 0;i <= 100;1++){
+  for (var i = 0;i <= 100;1++) {
     if (i === 0)
   console.log ('Beep!');
-console.log ('Boop!');
-console.log ("I'm Sorry, Dave.  I'm afraid I can't do that.")
-console.log(i)
-  }
-};
+  console.log ('Boop!');
+  console.log ("I'm Sorry, Dave.  I'm afraid I can't do that.")
+  console.log(i)
+    }
+  };

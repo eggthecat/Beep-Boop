4a65592ce06595757eb5fa90c318d78270ed0c94
Dominic Montelongo <montelnogodominic@gmail.com>
2019-04-05 11:01:08 -0700

    Realize misinterpretation of project; alter js accordingly - Project doesn't request for divisible properties, but requires unique recognition for numbers containing number in number.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
 function beepBoop(i){
   for (var i = 0;i <= 100;1++) {
-    if (i === 0)
+    if (i === 32) {
+      console.log ("I'm Sorry, Dave.  I'm afraid I can't do that.");
+    } else if (i === 21) {
+      console.log ('Boop!');
+    } else if (i === 13) {
+      console.log ("I'm Sorry, Dave.  I'm afraid I can't do that.");
+    } else if ((i === 1) && () {
+      console.log ('Beep!');
+    } else if (i === 2) {
+      console.log ('Boop!');
+    } else if (i === 3) {
+      console.log ("I'm Sorry, Dave.  I'm afraid I can't do that.");
+
+    if
+
+  console.log ('100')
   console.log ('Beep!');
   console.log ('Boop!');
   console.log ("I'm Sorry, Dave.  I'm afraid I can't do that.")
